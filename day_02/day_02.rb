ff7a50ee1f104104f73d207225063413c16d08e1
Paul Harker <paulh@sh24.org.uk>
2019-12-06 09:33:49 +0000

    salter added

--- day_02/day_02.rb
+++ day_02/day_02.rb
@@ -5,9 +5,21 @@ require_relative './compiler'
 
 class Day02
   def initialize(file)
-    instructions = FileLoader.new("./#{file}").to_a_from_line
-    @compiler = Compiler.new(instructions)
-    puts answer
+    @instructions = FileLoader.new("./#{file}").to_a_from_line
+    salter
+  end
+
+  def salter
+    (1..99).each do |num_x|
+      (1..99).each do |num_y|
+        @instructions[1] = num_x
+        @instructions[2] = num_y
+        @compiler = Compiler.new(@instructions.dup)
+        if answer == 19_690_720
+          puts (100*num_x) + num_y
+        end
+      end
+    end
   end
 
   def answer

0a86bbf7629d7acaddd20197d4f076fc77058b1d
Paul Harker <paulh@sh24.org.uk>
2019-12-10 14:11:31 +0000

    route builder working

--- day_03/route_builder.rb
+++ day_03/route_builder.rb
@@ -5,4 +5,52 @@ class RouteBuilder
   def initialize(file)
     @file = file
   end
+
+  def create_route
+    @file.inject([[0, 0]]) do |array, instruction|
+      array.concat perform_instruction(array.last.dup, instruction)
+    end
+  end
+
+  def perform_instruction(current_position, instruction)
+    direction = instruction[0]
+    instruction[0] = ''
+    number = instruction.to_i
+    case direction
+    when 'U'
+      perform_up(number, current_position)
+    when 'D'
+      perform_down(number, current_position)
+    when 'L'
+      perform_left(number, current_position)
+    when 'R'
+      perform_right(number, current_position)
+    end
+  end
+
+  private
+
+  def perform_up(number, current_position)
+    array = []
+    number.times { current_position[1] += 1; array << current_position.dup }
+    array
+  end
+
+  def perform_down(number, current_position)
+    array = []
+    number.times { current_position[1] -= 1; array << current_position.dup }
+    array
+  end
+
+  def perform_left(number, current_position)
+    array = []
+    number.times { current_position[0] -= 1; array << current_position.dup }
+    array
+  end
+
+  def perform_right(number, current_position)
+    array = []
+    number.times { current_position[0] += 1; array << current_position.dup }
+    array
+  end
 end
--- day_03/route_builder_test.rb
+++ day_03/route_builder_test.rb
@@ -3,11 +3,35 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class RouteBuilderTests < Minitest::Test
-  def setup
-    @route = RouteBuilder.new(['U3', 'L3', 'D3'])
-  end
 
   def test_file_input
+    @route = RouteBuilder.new(['U3', 'L3', 'D3'])
     assert_equal ['U3', 'L3', 'D3'], @route.file
   end
+
+  def test_create_route_up
+    @route = RouteBuilder.new(['U3'])
+    assert_equal [[0,0], [0,1], [0,2], [0,3]], @route.create_route
+  end
+
+  def test_create_route_down
+    @route = RouteBuilder.new(['D3'])
+    assert_equal [[0,0], [0,-1], [0,-2], [0,-3]], @route.create_route
+  end
+
+  def test_create_route_left
+    @route = RouteBuilder.new(['L3'])
+    assert_equal [[0,0], [-1,0], [-2,0], [-3,0]], @route.create_route
+  end
+
+  def test_create_route_right
+    @route = RouteBuilder.new(['R3'])
+    assert_equal [[0,0], [1,0], [2,0], [3,0]], @route.create_route
+  end
+
+  def test_multiple_commands
+    @route = RouteBuilder.new(['R1', 'U2'])
+    assert_equal [[0,0], [1,0], [1,1], [1,2]], @route.create_route
+  end
+
 end
